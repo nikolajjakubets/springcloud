47889b02c229cbcb0ee6e32ca54c8ba167e456d0
sq1990 <591036149@qq.com>
2018-11-19 14:09:52 +0800

    test

--- config-client/src/main/java/com/zpmc/config/client/controller/DemoController.java
+++ config-client/src/main/java/com/zpmc/config/client/controller/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 	
-	@Value("${demo.env}")
+	@Value("${dev}")
 	private String env;
 
 	@GetMapping("/hello")
