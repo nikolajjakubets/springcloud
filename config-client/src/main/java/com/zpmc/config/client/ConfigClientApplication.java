813ff89b15914b3a5ad3ba8b9081012df00f0901
sq1990 <591036149@qq.com>
2018-11-20 09:27:55 +0800

    test

--- config-client/src/main/java/com/zpmc/config/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/zpmc/config/client/ConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.zpmc.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
